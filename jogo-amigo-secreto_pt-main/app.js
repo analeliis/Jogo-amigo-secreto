46765a46e9b663a3b25c269f74a186212e8d57dc
Ana Clara Lélis <contato.anaclaralelis@gmail.com>
2025-02-15 19:30:18 -0300

    Funcionalidade para selecionar um amigo aleatorio

--- jogo-amigo-secreto_pt-main/app.js
+++ jogo-amigo-secreto_pt-main/app.js
@@ -31,3 +31,35 @@ function adicionarAmigo(){
 }
 
 //função para atulizar a lista de amigos da interface 
+function atualizarLista(){
+    const listaAmigos = document.getElementById('listaAmigos');
+
+    //Limpar o conteúdo atual da lista 
+    listaAmigos.innerHTML = ""; //Elimina qualquer conteúdo anterior da lista
+
+    //percorrer a matriz com um loop for
+    for(let i = 0; i <amigos.length; i++){
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
+//Funcionalidade para selecionar um amigo aleatorio
+function sortearAmigo(){
+    //validar se tem um amigo disponivel
+    if(amigos.length == 0){ //verifica se o vetor “amigos” está vazio
+        alert("Não a amigos disponiveis para sortear. Adicione pelo o menos um");
+        return;
+    }
+
+    //gerar um indice aleatorio
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    //obter o nome sorteado
+    const amigoSorteado = amigos[indiceAleatorio]; // usar o indice aleatorio para pegar um nome no array
+
+    //Mostrar o resultado no HTML
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong>`;
+}
