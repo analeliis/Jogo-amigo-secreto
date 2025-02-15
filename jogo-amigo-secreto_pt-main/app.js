44804c3e4c925aa11d671100ea5d217f6357dfca
Ana Clara Lélis <contato.anaclaralelis@gmail.com>
2025-02-15 19:50:15 -0300

    Atualizacao de um novo codigo porque a forma que fiz anteriormente não deu muito certo

--- jogo-amigo-secreto_pt-main/app.js
+++ jogo-amigo-secreto_pt-main/app.js
@@ -1,65 +1,61 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos =[];
+let nome = "";
 
-
-//Funcionalidade para agregar um amigo
-function adicionarAmigo(){
-    const inputAmigo = document.getElementById('amigo');
-    const nomeDoAmigo = inputAmigo
-
-    //validar se o campo não está vazio 
-    if(nome == ""){
-        alert("Por favo, insira um nome");
-        return;
-    }
-
-    //validação que o nome não esta dublicado
-    if(amigos.includes(nomeDoAmigo)){
-        alert(`O nome${nomeDoAmigo} ja esta na lista`);
-        return;
-    }
-
-    //Adicionar nome ao array de amigos
-    amigos.push(nomeDoAmigo);
-
-    //Limpar o campo de entrada
-    inputAmigo.ariaValu = "";
-
-    //Atualizar a lista no HTML
-    atualizarLista(); 
-
-}
-
-//função para atulizar a lista de amigos da interface 
-function atualizarLista(){
-    const listaAmigos = document.getElementById('listaAmigos');
-
-    //Limpar o conteúdo atual da lista 
-    listaAmigos.innerHTML = ""; //Elimina qualquer conteúdo anterior da lista
-
-    //percorrer a matriz com um loop for
-    for(let i = 0; i <amigos.length; i++){
-        const li = document.createElement('li');
-        li.textContent = amigos[i];
-        listaAmigos.appendChild(li);
+//criando função de salvar nome
+function salvarNome() {
+   nome = document.getElementById("nome").value;
+    if (nome == "") {
+        alert("Por favor, insira seu nome");
+    } else {
+        listaDeAmigos.push(nome);
+        document.getElementById("nome").value = "";
+        atualizarLista();
     }
 }
 
-//Funcionalidade para selecionar um amigo aleatorio
-function sortearAmigo(){
-    //validar se tem um amigo disponivel
-    if(amigos.length == 0){ //verifica se o vetor “amigos” está vazio
-        alert("Não a amigos disponiveis para sortear. Adicione pelo o menos um");
+// criando lista de amigos vazia
+let listaDeAmigos = [];
+
+//criando função de adicionar amigo na lista
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value;
+    if (nomeAmigo == "") {
+        alert("Por favor, insira um nome");
+    } else {
+        listaDeAmigos.push(nomeAmigo);
+        document.getElementById("amigo").value ="";
+        atualizarLista();
+    }
+}
+// funcão para atualizar a lista de amigos
+function atualizarLista() {
+    let listaAtualizada = document.getElementById("listaAmigos");
+    listaAtualizada.innerHTML = "";
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let itemLista = document.createElement("li");
+        itemLista.innerHTML = listaDeAmigos[i];
+        listaAtualizada.appendChild(itemLista);
+    }
+}
+// funcão para sortear amigo e também impedir que a pessoa tire seu proprio nome
+function sortearAmigo() {
+    if (listaDeAmigos.length == 0) {
+        alert("Por favor, insira um nome.");
+        return;
+
+    }
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let nomeSorteado = listaDeAmigos[indiceAleatorio];
+    if (nomeSorteado == nome) {
+        alert("Você não pode ser seu próprio amigo secreto! Por favor, tente novamente.");
         return;
     }
 
-    //gerar um indice aleatorio
-    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    document.getElementById("resultado").innerHTML = `Seu amigo secreto é: ${nomeSorteado}`;
 
-    //obter o nome sorteado
-    const amigoSorteado = amigos[indiceAleatorio]; // usar o indice aleatorio para pegar um nome no array
+}  
 
-    //Mostrar o resultado no HTML
-    const resultado = document.getElementById('resultado');
-    resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong>`;
+function reiniciarSorteio() {
+    listaDeAmigos = [];
+    document.getElementById("resultado").innerHTML = "";
+    atualizarLista();
 }
