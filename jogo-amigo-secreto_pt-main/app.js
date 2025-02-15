7357ec25fa1e799221a6fc42f2ba7dcbbf5d5260
Ana Clara Lélis <contato.anaclaralelis@gmail.com>
2025-02-15 19:12:57 -0300

    inserindo as funcionalidade de agrregar um amigo validar o nome não duplicado e adicionar ao campo matriz amigo

--- jogo-amigo-secreto_pt-main/app.js
+++ jogo-amigo-secreto_pt-main/app.js
@@ -1,6 +1,33 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos =[];
 
+
+//Funcionalidade para agregar um amigo
 function adicionarAmigo(){
-    let campo = document.querySelector();
+    const inputAmigo = document.getElementById('amigo');
+    const nomeDoAmigo = inputAmigo
+
+    //validar se o campo não está vazio 
+    if(nome == ""){
+        alert("Por favo, insira um nome");
+        return;
+    }
+
+    //validação que o nome não esta dublicado
+    if(amigos.includes(nomeDoAmigo)){
+        alert(`O nome${nomeDoAmigo} ja esta na lista`);
+        return;
+    }
+
+    //Adicionar nome ao array de amigos
+    amigos.push(nomeDoAmigo);
+
+    //Limpar o campo de entrada
+    inputAmigo.ariaValu = "";
+
+    //Atualizar a lista no HTML
+    atualizarLista();
+
+
+
 }
